afa2222616473d45993588c42fa2b228e44ba8a2
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-06-25 11:26:29 +0100

    feat: refactor MealsScreen component to load and display meals dynamically

--- app/(tabs)/meals.tsx
+++ app/(tabs)/meals.tsx
@@ -1,11 +1,49 @@
-import { globalStyles } from '@/assets/styles/global.styles'
-import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import { globalStyles } from '@/assets/styles/global.styles';
+import { useFocusEffect } from 'expo-router';
+import React, { useCallback, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import MealItem from '../component/MealItem';
+import { getMeals, Meal } from '../storage/meals';
 
-export default function meals() {
+export default function MealsScreen() {
+    const [meals, setMeals] = useState<Meal[]>([]);
+
+    const loadMeals = async () => {
+        const data = await getMeals();
+        setMeals(data);
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            loadMeals();
+        }, [])
+    );
+
     return (
         <ScrollView style={globalStyles.container}>
             <Text style={globalStyles.title}>All meals</Text>
+            <View style={{ marginTop: 30 }}>
+
+                {meals.length === 0 ? (
+
+                    <Text style={globalStyles.empty}>No Meals logged yet.</Text>
+                ) :
+                    (
+                        meals.map((meal) => (
+                            <MealItem
+                                key={meal.id}
+                                id={meal.id}
+                                name={meal.name}
+                                calories={meal.calories}
+                                protein={meal.protein}
+                                crabs={meal.carbs}
+                                fat={meal.fat}
+                                onDelete={loadMeals}
+                            />
+                        ))
+                    )
+                }
+            </View>
         </ScrollView>
     )
 }
